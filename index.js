8f28bcc610b7fcb12f299d8ba44138c0689e34c3
Evan Carroll <me@evancarroll.com>
2014-01-20 18:22:16 -0600

    removed the one-off caching and now we don't fire F until we get the CB

--- index.js
+++ index.js
@@ -18,28 +18,11 @@ var arr_cast = Array.prototype.slice;
 function thunkify(fn){
   return function(){
     var args = arr_cast.call(arguments);
-    var results;
-    var called;
-    var cb;
 
-    args.push(function(){
-      results = arguments;
-
-      if (cb && !called) {
-        called = true;
-        cb.apply(this, results);
-      }
-    });
-
-    fn.apply(this, args);
-
-    return function(fn){
-      cb = fn;
-
-      if (results && !called) {
-        called = true;
-        fn.apply(this, results);
-      }
+    return function(cb){
+			args.push(cb);
+			fn.apply(this, args);
     }
-  }
+
+	}
 };
